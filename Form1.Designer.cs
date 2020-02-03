7f9bcc3292a7529dcd8a6af3bdacb621ad03a49a
HaciIsma <haciismayilov5@outlook.com>
2020-02-03 04:54:07 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace WORDPAD_KILLER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.Save = new MetroFramework.Controls.MetroButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ColorLab = new MetroFramework.Controls.MetroLabel();
+            this.Load = new MetroFramework.Controls.MetroButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Colorcmb = new MetroFramework.Controls.MetroComboBox();
+            this.alignmentLab = new MetroFramework.Controls.MetroLabel();
+            this.Right = new System.Windows.Forms.Button();
+            this.Italic = new System.Windows.Forms.Button();
+            this.Center = new System.Windows.Forms.Button();
+            this.Left = new System.Windows.Forms.Button();
+            this.Underline = new System.Windows.Forms.Button();
+            this.Bold = new System.Windows.Forms.Button();
+            this.frontStyle = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel4 = new MetroFramework.Controls.MetroPanel();
+            this.sizeLab = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel3 = new MetroFramework.Controls.MetroPanel();
+            this.FontLab = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.sizecmbox = new MetroFramework.Controls.MetroComboBox();
+            this.Fontcmbox = new MetroFramework.Controls.MetroComboBox();
+            this.Text = new System.Windows.Forms.RichTextBox();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.Save);
+            this.metroPanel1.Controls.Add(this.textBox2);
+            this.metroPanel1.Controls.Add(this.ColorLab);
+            this.metroPanel1.Controls.Add(this.Load);
+            this.metroPanel1.Controls.Add(this.textBox1);
+            this.metroPanel1.Controls.Add(this.Colorcmb);
+            this.metroPanel1.Controls.Add(this.alignmentLab);
+            this.metroPanel1.Controls.Add(this.Right);
+            this.metroPanel1.Controls.Add(this.Italic);
+            this.metroPanel1.Controls.Add(this.Center);
+            this.metroPanel1.Controls.Add(this.Left);
+            this.metroPanel1.Controls.Add(this.Underline);
+            this.metroPanel1.Controls.Add(this.Bold);
+            this.metroPanel1.Controls.Add(this.frontStyle);
+            this.metroPanel1.Controls.Add(this.metroPanel4);
+            this.metroPanel1.Controls.Add(this.sizeLab);
+            this.metroPanel1.Controls.Add(this.metroPanel3);
+            this.metroPanel1.Controls.Add(this.FontLab);
+            this.metroPanel1.Controls.Add(this.metroPanel2);
+            this.metroPanel1.Controls.Add(this.sizecmbox);
+            this.metroPanel1.Controls.Add(this.Fontcmbox);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(0, 25);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(846, 60);
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(766, 34);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(617, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // ColorLab
+            // 
+            this.ColorLab.AutoSize = true;
+            this.ColorLab.Location = new System.Drawing.Point(500, 6);
+            this.ColorLab.Name = "ColorLab";
+            this.ColorLab.Size = new System.Drawing.Size(42, 19);
+            this.ColorLab.TabIndex = 1;
+            this.ColorLab.Text = "Color";
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(766, 6);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 1;
+            this.Load.Text = "Load";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(617, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Colorcmb
+            // 
+            this.Colorcmb.FormattingEnabled = true;
+            this.Colorcmb.ItemHeight = 23;
+            this.Colorcmb.Location = new System.Drawing.Point(444, 28);
+            this.Colorcmb.Name = "Colorcmb";
+            this.Colorcmb.Size = new System.Drawing.Size(149, 29);
+            this.Colorcmb.TabIndex = 1;
+            // 
+            // alignmentLab
+            // 
+            this.alignmentLab.AutoSize = true;
+            this.alignmentLab.Location = new System.Drawing.Point(337, 6);
+            this.alignmentLab.Name = "alignmentLab";
+            this.alignmentLab.Size = new System.Drawing.Size(69, 19);
+            this.alignmentLab.TabIndex = 1;
+            this.alignmentLab.Text = "Alignment";
+            // 
+            // Right
+            // 
+            this.Right.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Right.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Right.Location = new System.Drawing.Point(394, 28);
+            this.Right.Name = "Right";
+            this.Right.Size = new System.Drawing.Size(29, 29);
+            this.Right.TabIndex = 6;
+            this.Right.Text = "R";
+            this.Right.UseVisualStyleBackColor = true;
+            // 
+            // Italic
+            // 
+            this.Italic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Italic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Italic.Location = new System.Drawing.Point(276, 28);
+            this.Italic.Name = "Italic";
+            this.Italic.Size = new System.Drawing.Size(29, 29);
+            this.Italic.TabIndex = 3;
+            this.Italic.Text = "I";
+            this.Italic.UseVisualStyleBackColor = true;
+            // 
+            // Center
+            // 
+            this.Center.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Center.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Center.Location = new System.Drawing.Point(359, 28);
+            this.Center.Name = "Center";
+            this.Center.Size = new System.Drawing.Size(29, 29);
+            this.Center.TabIndex = 5;
+            this.Center.Text = "C";
+            this.Center.UseVisualStyleBackColor = true;
+            // 
+            // Left
+            // 
+            this.Left.BackColor = System.Drawing.Color.White;
+            this.Left.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Left.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Left.Location = new System.Drawing.Point(324, 28);
+            this.Left.Name = "Left";
+            this.Left.Size = new System.Drawing.Size(29, 29);
+            this.Left.TabIndex = 4;
+            this.Left.Text = "L";
+            this.Left.UseVisualStyleBackColor = false;
+            // 
+            // Underline
+            // 
+            this.Underline.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Underline.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Underline.Location = new System.Drawing.Point(241, 28);
+            this.Underline.Name = "Underline";
+            this.Underline.Size = new System.Drawing.Size(29, 29);
+            this.Underline.TabIndex = 2;
+            this.Underline.Text = "U";
+            this.Underline.UseVisualStyleBackColor = true;
+            // 
+            // Bold
+            // 
+            this.Bold.BackColor = System.Drawing.Color.White;
+            this.Bold.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bold.Location = new System.Drawing.Point(206, 28);
+            this.Bold.Name = "Bold";
+            this.Bold.Size = new System.Drawing.Size(29, 29);
+            this.Bold.TabIndex = 1;
+            this.Bold.Text = "B";
+            this.Bold.UseVisualStyleBackColor = false;
+            // 
+            // frontStyle
+            // 
+            this.frontStyle.AutoSize = true;
+            this.frontStyle.Location = new System.Drawing.Point(220, 6);
+            this.frontStyle.Name = "frontStyle";
+            this.frontStyle.Size = new System.Drawing.Size(71, 19);
+            this.frontStyle.TabIndex = 1;
+            this.frontStyle.Text = "Front Style";
+            // 
+            // metroPanel4
+            // 
+            this.metroPanel4.HorizontalScrollbarBarColor = true;
+            this.metroPanel4.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel4.HorizontalScrollbarSize = 10;
+            this.metroPanel4.Location = new System.Drawing.Point(276, 28);
+            this.metroPanel4.Name = "metroPanel4";
+            this.metroPanel4.Size = new System.Drawing.Size(0, 0);
+            this.metroPanel4.TabIndex = 2;
+            this.metroPanel4.VerticalScrollbarBarColor = true;
+            this.metroPanel4.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel4.VerticalScrollbarSize = 10;
+            // 
+            // sizeLab
+            // 
+            this.sizeLab.AutoSize = true;
+            this.sizeLab.Location = new System.Drawing.Point(149, 6);
+            this.sizeLab.Name = "sizeLab";
+            this.sizeLab.Size = new System.Drawing.Size(32, 19);
+            this.sizeLab.TabIndex = 4;
+            this.sizeLab.Text = "Size";
+            // 
+            // metroPanel3
+            // 
+            this.metroPanel3.HorizontalScrollbarBarColor = true;
+            this.metroPanel3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.HorizontalScrollbarSize = 10;
+            this.metroPanel3.Location = new System.Drawing.Point(241, 28);
+            this.metroPanel3.Name = "metroPanel3";
+            this.metroPanel3.Size = new System.Drawing.Size(0, 0);
+            this.metroPanel3.TabIndex = 2;
+            this.metroPanel3.VerticalScrollbarBarColor = true;
+            this.metroPanel3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.VerticalScrollbarSize = 10;
+            // 
+            // FontLab
+            // 
+            this.FontLab.AutoSize = true;
+            this.FontLab.Location = new System.Drawing.Point(39, 6);
+            this.FontLab.Name = "FontLab";
+            this.FontLab.Size = new System.Drawing.Size(35, 19);
+            this.FontLab.TabIndex = 1;
+            this.FontLab.Text = "Font";
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(206, 28);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(0, 0);
+            this.metroPanel2.TabIndex = 1;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // sizecmbox
+            // 
+            this.sizecmbox.FormattingEnabled = true;
+            this.sizecmbox.ItemHeight = 23;
+            this.sizecmbox.Items.AddRange(new object[] {
+            "8 ",
+            "9 ",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "36",
+            "48",
+            "72"});
+            this.sizecmbox.Location = new System.Drawing.Point(143, 28);
+            this.sizecmbox.Name = "sizecmbox";
+            this.sizecmbox.Size = new System.Drawing.Size(43, 29);
+            this.sizecmbox.TabIndex = 3;
+            // 
+            // Fontcmbox
+            // 
+            this.Fontcmbox.FormattingEnabled = true;
+            this.Fontcmbox.ItemHeight = 23;
+            this.Fontcmbox.Location = new System.Drawing.Point(3, 28);
+            this.Fontcmbox.Name = "Fontcmbox";
+            this.Fontcmbox.Size = new System.Drawing.Size(121, 29);
+            this.Fontcmbox.TabIndex = 2;
+            // 
+            // Text
+            // 
+            this.Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text.Location = new System.Drawing.Point(3, 88);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(838, 542);
+            this.Text.TabIndex = 1;
+            this.Text.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 636);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "Form1";
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroComboBox Fontcmbox;
+        private MetroFramework.Controls.MetroComboBox sizecmbox;
+        private MetroFramework.Controls.MetroLabel FontLab;
+        private MetroFramework.Controls.MetroPanel metroPanel4;
+        private MetroFramework.Controls.MetroLabel sizeLab;
+        private MetroFramework.Controls.MetroPanel metroPanel3;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroLabel frontStyle;
+        private System.Windows.Forms.Button Bold;
+        private System.Windows.Forms.Button Underline;
+        private System.Windows.Forms.Button Italic;
+        private System.Windows.Forms.Button Right;
+        private System.Windows.Forms.Button Center;
+        private System.Windows.Forms.Button Left;
+        private MetroFramework.Controls.MetroLabel alignmentLab;
+        private MetroFramework.Controls.MetroComboBox Colorcmb;
+        private MetroFramework.Controls.MetroLabel ColorLab;
+        private MetroFramework.Controls.MetroButton Save;
+        private System.Windows.Forms.TextBox textBox2;
+        private MetroFramework.Controls.MetroButton Load;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox Text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using MetroFramework.Forms;
+using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace WORDPAD_KILLER
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FrontcmbAdd();
+            ColorcmbAdd();
+        }
+
+        private void FrontcmbAdd()
+        {
+            foreach (FontFamily font in FontFamily.Families)
+            {
+                Fontcmbox.Items.Add(font.Name.ToString());
+            }
+        }
+        private void ColorcmbAdd()
+        {
+            foreach (var color in Enum.GetNames(typeof(System.Drawing.KnownColor)))
+            {
+                Colorcmb.Items.Add(color.ToString());
+            }
+        }
+      
+    }
+}
